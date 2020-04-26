ddf33295ec359d1da8d2578e6c2b502de3b5e6ea
Haley Griffin <haleygriffin708@gmail.comgit config --global user.name h-griffin>
2020-04-25 18:36:38 -0700

    cannot PUT books, everything else working but update delete

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.set('view engine', 'ejs');
 //static routs
 app.use(express.static('public'));
 
-//volhas constructor
 function Book(data){
   const placeHolderImage ='https://i.imgur.com/J5LVHEL.jpg';
   let httpRegex = /^(http:\/\/)/g;
@@ -41,34 +40,17 @@ function Book(data){
   this.bookshelf = data.categories ? data.categories : 'no bookshelf';
 }
 
-app.get('/hello', (request, response) => {
-  response.send('hello from ejs');
-});
-
-app.get('/searches/new', (request, response) => {
-  response.render('./pages/searches/new');
-});
-
 function bookHandler(request, response){
   let queryType = request.body.search[0];
   let queryTerms = request.body.search[1];
-  let url = `https://www.googleapis.com/books/v1/volumes?q=+in${queryTerms}:${queryType}`; //google api
-  superagent.get(url) //returns promise
+  let url = `https://www.googleapis.com/books/v1/volumes?q=+in${queryTerms}:${queryType}`;
+  superagent.get(url)
     .then(results => results.body.items.map(book => new Book(book.volumeInfo)))
-    .then(book => response.render('./pages/searches/show', {book : book})) //send- needs to be render later on to show
+    .then(book => response.render('./pages/searches/show', {book : book}))
     .catch(error => {
       errorHandler('book handler error: superagent', request, response);
     });
 }
-
-app.post('/searches', bookHandler);
-
-// app.get('/', (request, response) => {
-//   response.render('./pages/index.ejs');
-// });
-
-//jacob something with database    / this home page '/'
-app.get('/', renderHomePage);
 
 function renderHomePage(request ,response){
   let selectQuery = `SELECT * FROM books;`;
@@ -86,14 +68,27 @@ function renderHomePage(request ,response){
     });
 }
 
-//jacob /view saved books
-app.get('/books/:id', savedBooks);
+function saveBook(request, response){
+  const { title, author, description, isbn, image_url, bookshelf } = request.body;
+
+  let addBookSQL = `INSERT INTO books (title, author, description, isbn, image_url, bookshelf) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
+  let addBookValues = [title, author, description, isbn, image_url, bookshelf];
+
+  dbClient.query(addBookSQL, addBookValues)
+    .then(data => {
+      console.log(data.rows);
+      response.render('pages/details', {book: data.rows[0]});
+    })
+    .catch(error => {
+      errorHandler('somethings bad over here', request, response);
+    });
+}
 
 function savedBooks(request, response){
-  const bookId = request.params.id; //attach data and influence rout
+  const bookId = request.params.id;
 
-  console.log(request.query); // url query stings : ?key:value
-  console.log(request.body); //attaching header to request? sending as object not just string
+  console.log(request.query);
+  console.log(request.body);
 
   let selectQuery = `SELECT * FROM books WHERE id=$1;`;
   let selectValues = [bookId];
@@ -105,29 +100,7 @@ function savedBooks(request, response){
     });
 }
 
-//if no book id
-app.get('books', (request,response) => response.send('no id present'));
-
-//insert api info into DB / save book / after click save book
-app.post('/books', (request, response) => {
-  const { title, author, description, isbn, image_url, bookshelf } = request.body;
-
-  let addBookSQL = `INSERT INTO books (title, author, description, isbn, image_url, bookshelf) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
-  let addBookValues = [title, author, description, isbn, image_url, bookshelf];
-  // console.log( addBookSQL, addBookValues);
-  dbClient.query(addBookSQL, addBookValues)
-    .then(data => {
-      console.log(data.rows);
-      // response.send('adding book in progress');
-      response.render('pages/details', {book: data.rows[0]});
-    })
-    .catch(error => {
-      errorHandler('somethings bad over here', request, response);
-    });
-});
-
-//update a resource / database
-app.put('/books/:id', (request, response) => {
+function updateBook(request, response){
   const bookId = request.params.id;
   const { title, author, description, image_url, isbn, bookshelf} = request.body;
   console.log(request.body.title);
@@ -137,7 +110,6 @@ app.put('/books/:id', (request, response) => {
   let values = [title, author, description, image_url, isbn, bookshelf, bookId];
 
   //use SQL UPDATE WHERE to modify the row record
-
   dbClient.query(SQL, values)
     .then(data => {
       response.send(data); //render later
@@ -150,7 +122,24 @@ app.put('/books/:id', (request, response) => {
 
   //invalidate old thing and replace with new
   response.send(bookId);
+}
+
+app.get('/hello', (request, response) => {
+  response.send('hello from ejs');
 });
+
+app.get('/searches/new', (request, response) => {
+  response.render('./pages/searches/new');
+});
+
+//if no book id
+app.get('books', (request,response) => response.send('no id present'));
+
+app.get('/', renderHomePage);
+app.post('/searches', bookHandler);
+app.post('/books', saveBook);
+app.get('/books/:id', savedBooks);
+app.put('/books/:id', updateBook);
 
 function errorHandler(error, request, response){
   console.log(error);
